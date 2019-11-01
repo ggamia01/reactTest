10996b3214084adae52a5e1fcbd029ea8a003a61
song <gganggilove@naver.com>
2019-11-01 17:18:06 +0900

    reactTest1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
+const name = 'react';
+const value = 1;
+const style ={
+  backgroundColor : 'black',
+  paddig: '16px',
+  color: 'white',
+  fontSize: '25px'
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        Hello {name}!
+      </div>
+      <div>
+        {
+          1+1 === 3
+            ? (<div>맞아요!</div>)
+            : (<div>틀려요!</div>)
+        }
+      </div>
+      <div>
+        {
+          1+1 === 2 && (<div>맞아요!</div>)
+        }
+      </div>
+      <div>
+        {
+          (function(){
+            if (value === 1) return (<div>하나</div>);
+            if (value === 2) return (<div>둘</div>);
+            if (value === 3) return (<div>셋</div>);
+          })()     
+        }
+      </div>
+      <div>
+        {
+          (() => {
+                if (value === 1) return (<div>하나</div>);
+                if (value === 2) return (<div>둘</div>);
+                if (value === 3) return (<div>셋</div>);
+          })()
+        }
+      </div>
+      <div style={style}>
+        안녕하세요!
+      </div>
+      <div className="App">
+        리액트
+      </div>
     </div>
+      
   );
 }
 
